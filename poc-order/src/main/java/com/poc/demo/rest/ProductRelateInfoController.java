af386b715f691b9fec6f26384e9a88d2b18e57da
tutu <30116992@qq.com>
2019-10-26 14:23:57 +0800

    优化

--- poc-order/src/main/java/com/poc/demo/rest/ProductRelateInfoController.java
+++ poc-order/src/main/java/com/poc/demo/rest/ProductRelateInfoController.java
@@ -1,22 +1,12 @@
 package com.poc.demo.rest;
 
-import com.alibaba.fastjson.JSONObject;
 import com.github.wxiaoqi.security.common.rest.BaseController;
-import com.poc.demo.biz.ProductInfoBiz;
 import com.poc.demo.biz.ProductRelateInfoBiz;
-import com.poc.demo.entity.ProductInfo;
 import com.poc.demo.entity.ProductRelateInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
-import java.util.List;
 
 @RestController
 @RequestMapping("productRelateInfo")

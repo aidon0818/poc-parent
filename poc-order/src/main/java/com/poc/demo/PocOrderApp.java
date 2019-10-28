480dc8aaa8083427962486e5a6b0018515aa8257
tutu <30116992@qq.com>
2019-10-28 15:13:18 +0800

    优化

--- poc-order/src/main/java/com/poc/demo/PocOrderApp.java
+++ poc-order/src/main/java/com/poc/demo/PocOrderApp.java
@@ -5,13 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.spring4all.swagger.EnableSwagger2Doc;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
  * Created by ace on 2017/7/29.
  */
 @SpringBootApplication
-//@EnableDiscoveryClient
-//@RefreshScope
+@EnableDiscoveryClient
+@RefreshScope
 @EnableSwagger2Doc
 @MapperScan("com.poc.demo.mapper")
 public class PocOrderApp {
